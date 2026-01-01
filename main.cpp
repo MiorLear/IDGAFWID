61492ad65842cf7c8d0cfdf815a6169450037d3c
miorlear <miguelledezma005@gmail.com>
2026-01-01 15:00:46 -0600

    Added debugging features using imgui

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
-
+#include "imgui.h"
+#include "rlImGui.h"
 int main() {
     //Initialization
     const int screenWidth = 1600;
@@ -8,9 +9,10 @@ int main() {
     InitWindow(screenWidth, screenHeight, "Raylib Attemp1");
 
     //Define the character
-    Vector2 ballPosition = {.x=(float)screenWidth/2, .y=(float)screenHeight/2};
+    Vector2 ballPosition = {(float)screenWidth/2, (float)screenHeight/2};
     float ballSpeed = 1.0f;
 
+    rlImGuiSetup(true);
     //Main Loop
     while (!WindowShouldClose()) {
         //Module 1 - (movement)
@@ -20,13 +22,23 @@ int main() {
         if (IsKeyDown(KEY_DOWN)) ballPosition.y += ballSpeed;
 
         //Module 2 - Drawing
-
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("Move the ball with the keys!", 10, 10, 20, DARKGRAY);
             DrawCircleV(ballPosition, 40, MAROON);
+
+            rlImGuiBegin();
+                // ToolKit
+                ImGui::Begin("Control de Experimento");
+                ImGui::SliderFloat("Posicion X", &ballPosition.x, 0, 1600);
+                ImGui::SliderFloat("Velocidad", &ballSpeed, 0, 4);
+                ImGui::Text("FPS: %i", GetFPS());
+                ImGui::End();
+            rlImGuiEnd();
         EndDrawing();
     }
+
+    rlImGuiShutdown();
     CloseWindow();
     return 0;
 }
